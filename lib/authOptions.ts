b907b0fa95d3566eeacbf2a613414938e9ef16d0
Wolf1610 <dutta2772004@gmail.com>
2025-07-19 10:46:37 +0530

    move authOption to a seperate file

---
+++ lib/authOptions.ts
@@ -0,0 +1,58 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "@/lib/prisma";
+import { JWT } from "next-auth/jwt";
+import { Session } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async signIn({ profile }) {
+      if (!profile?.email) throw new Error("No email found in profile");
+
+      await prisma.user.upsert({
+        where: { email: profile.email },
+        create: {
+          email: profile.email,
+          name: profile.name,
+          image: profile.image,
+        },
+        update: {
+          name: profile.name,
+          image: profile.image,
+        },
+      });
+
+      return true;
+    },
+    async jwt({ token, profile }) {
+      if (profile?.email) {
+        const user = await prisma.user.findUnique({
+          where: { email: profile.email },
+        });
+        if (user) token.id = user.id;
+      }
+      return token;
+    },
+    async session({
+      session,
+      token,
+    }: {
+      session: Session;
+      token: JWT;
+    }): Promise<Session> {
+      if (session.user && token.id) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
+  },
+};
